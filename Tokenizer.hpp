f938f2cbcf35587bfd5f2401f0b5f06268df93e1
DAWS232 <159370362+DAWS232@users.noreply.github.com>
2024-02-24 15:24:34 -0800

    Create Tokenizer.hpp

---
+++ Tokenizer.hpp
@@ -0,0 +1,22 @@
+#ifndef TOKENIZERPROJECT_TOKENIZER_HPP
+#define TOKENIZERPROJECT_TOKENIZER_HPP
+
+#include <cctype>
+#include <string>
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <fstream>
+#include "Token.hpp"
+
+
+class Tokenizer {
+public:
+    void printTokens();
+    std::vector<Token> tokenize(std::string input);
+private:
+    std::vector<Token> tokens;
+};
+
+
+#endif //TOKENIZERPROJECT_TOKENIZER_HPP
